c520332cb11e0df525a05c193979b3de1f1776be
Daniel Paulino <daniel-paulino@hotmail.com>
2025-02-25 00:19:13 -0300

    feat: add GenericFeignException

---
+++ src/main/java/br/com/userservice/commonslib/model/exceptions/GenericFeignException.java
@@ -0,0 +1,15 @@
+package br.com.userservice.commonslib.model.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GenericFeignException extends RuntimeException {
+    private Integer status;
+    private Map error;
+}
